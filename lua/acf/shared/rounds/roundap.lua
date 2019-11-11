7863759ef3fb4ef624d8d872ade7e0907675a957
ASBESTOS-1 <gettysburg97EON@gmail.com>
2019-11-11 08:44:45 -0500

    fixed a git issue

--- lua/acf/shared/rounds/roundap.lua
+++ lua/acf/shared/rounds/roundap.lua
@@ -1,7 +1,7 @@
 
 AddCSLuaFile()
 
-ACF.AmmoBlacklist.AP = { "MO", "SL" }
+ACF.AmmoBlacklist.AP = { "MO", "SL", "SB" }
 
 local Round = {}
 
--- lua/acf/shared/rounds/roundheat.lua
+++ lua/acf/shared/rounds/roundheat.lua
@@ -1,7 +1,7 @@
 
 AddCSLuaFile()
 
-ACF.AmmoBlacklist.HEAT = { "MG", "HMG", "RAC", "AC", "SL" }
+ACF.AmmoBlacklist.HEAT = { "MG", "HMG", "RAC", "AC", "SL" , "SB" }
 
 local Round = {}
 
